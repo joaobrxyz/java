c7530f88d65394a60b4d84e5180e35ed2189efb5
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-02-08 17:18:30 -0300

    ex002

---
+++ ex002/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double tempCelsius = 30;
+        double tempFahrenheit = tempCelsius * 1.8 + 32;
+        System.out.println("A temperatura em celsius é: " + tempCelsius + "°, em fahrenheit é: " + tempFahrenheit + "°");
+    }
+}
