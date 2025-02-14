9d8bac2dd707a168dc305a598425917170e9a83a
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-02-14 20:41:41 -0300

    Aula 13

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,26 +1,28 @@
+package br.com.alura.screenmatch.modelos;
+
 public class Filme {
     String nome;
     int anoDeLancamento;
-    boolean incluidoNoPlano;
+    public boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
-    int getTotalDeAvaliacoes(){
+    public int getTotalDeAvaliacoes(){
         return totalDeAvaliacoes;
     }
 
-    void exibeFichaTecnica() {
+    public void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
-    void avalia(double nota) {
+    public void avalia(double nota) {
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    double pegaMedia(){
+    public double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
