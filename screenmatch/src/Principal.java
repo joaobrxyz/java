fb91ac93d9b857aae6f0c2a645468a2988c69dc3
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-02-17 20:31:29 -0300

    Aula 21
    Entendendo a hierarquia de classes e métodos

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -66,7 +66,8 @@ public class Principal {
         listaDeFilmes.add(outroFilme);
 
         System.out.println("Tamanho da lista: " + listaDeFilmes.size());
-        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println("Primeiro filme: " + listaDeFilmes.getFirst().getNome());
         System.out.println(listaDeFilmes);
+        System.out.println("toString do filme: : " + listaDeFilmes.getFirst().toString());
     }
 }
