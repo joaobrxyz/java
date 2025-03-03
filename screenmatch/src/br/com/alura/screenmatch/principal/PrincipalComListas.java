2546647ee498c9de5127a7d482f11500c56bfb48
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-03-03 17:23:17 -0300

    Aula 27
    Definindo outros critérios de ordenação

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -43,6 +44,9 @@ public class PrincipalComListas {
 
         Collections.sort(lista);
         System.out.println("Lista de títulos ordenados");
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
         System.out.println(lista);
     }
 }
