615eb0b955f37f1e353c7ab89df395a96562ac55
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-03-05 20:29:31 -0300

    Aula 29
    Refinando a busca por filmes

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite um filme para buscar: ");
+        var buscar = input.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + buscar + "&apikey=74a96322";
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=74a96322")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
 
