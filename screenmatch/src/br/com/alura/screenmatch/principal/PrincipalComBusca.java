1b309e908872b11424954613d41aec5deaf6fdff
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-04-07 20:40:35 -0300

    Aula 32
    Tratando diversas exceções

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -21,18 +21,30 @@ public class PrincipalComBusca {
 
         String endereco = "https://www.omdbapi.com/?t=" + buscar + "&apikey=74a96322";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-        String json = response.body();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+            String json = response.body();
 
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
 
-        System.out.println("Título já convertido");
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println(meuTitulo);
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Título já convertido");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Algum erro de argumento na busca, verifique o endereço");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("O programa finalizou corretamente!");
+
+
+
     }
 }
